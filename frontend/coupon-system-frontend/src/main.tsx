6f5b8f5a8fdc9d903db88fa5dc2e108bb6e7bd8e
ebinthomas06 <ebinthomas1810@gmail.com>
2025-12-11 16:53:09 +0530

    adminpage,student dashboard complete

--- frontend/coupon-system-frontend/src/main.tsx
+++ frontend/coupon-system-frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from '../src/context/AuthContext.tsx';
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const GOOGLE_CLIENT_ID = "997232015364-urvnif0r9aacvtlslfu1kurbrps9jb0h.apps.googleusercontent.com"; 
+const GOOGLE_CLIENT_ID = ".apps.googleusercontent.com"; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
